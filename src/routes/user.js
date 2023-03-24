d7662fa1d53b74ce0831652ddccb4b5b08ddb14d
fer20210727 <128200322+fer20210727@users.noreply.github.com>
2023-03-24 10:04:20 -0600

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -8,10 +8,9 @@ const router = express.Router()
 router.post('/users', (req, res) => {
   const user = userSchema(req.body)
   user
-    
+    .save()
     .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error }))
-  .save();
+    .catch((error) => res.json({ message: error }));
   
 })
 
